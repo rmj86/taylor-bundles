1b05bb3c40d55a3e6524ae50c59c8bb28d4dd8ed
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-15 19:58:24 +0200

    mixer takes array arguments
    Changed the color mixer to take numpy array arguments. Note that it no
    longer works with plain number args!

--- colormix.py
+++ colormix.py
@@ -5,7 +5,8 @@ Created on Wed Feb 17 22:45:14 2016
 @author: hannes
 """
 
-from numpy import pi, cos, sqrt, array
+from numpy import pi, cos, sqrt, array, newaxis
+from matplotlib.colors import colorConverter
 
 tau = 2*pi
 
@@ -15,19 +16,31 @@ def constant(color):
     return mix
 
 def cosine2(color1, color2, u, v, linear=False):
-    """ returns a function mix(t) which mixes colors c1 and c2. c1 and c2 are 
-        RGB 3-tuplets (The length is not checked). u is the peak of c1, and v 
-        is the peak of c2. To mix in a liear color space, use linear=True."""
+    """ returns a function mix(t) which mixes colors c1 and c2. c1 and c2
+        are  any valid matplotlib color. E.g. a color char ('r'), a html
+        color name ("red"), a html hex color ("#FF0000") or a 3- or 4-tuple.
+        u is the peak of c1, and v is the peak of c2. To mix in a liear
+        color space, use linear=True.
+        The returned mix function takes a numpy array with shape (n,) and
+        returns a numpy array with shape (n,4) - rgba."""
     period = 2*(v-u)
-    c1, c2 = array(color1), array(color2)
+    const = tau/period
+    c1 = colorConverter.to_rgba_array(color1)
+    c2 = colorConverter.to_rgba_array(color2)
     if linear:
         def mix(t):
-            m = 0.5 * (1 + cos((t-u) * tau/period))
+            m = 0.5 * (1 + cos((t-u) * const))
+            m = m[:,newaxis]
             return sqrt(m*c1**2 + (1-m)*c2**2)
     else:
         def mix(t):
-            m = 0.5 * (1 + cos((t-u) * tau/period))
+            # TODO: should take numpy array argument
+            m = 0.5 * (1 + cos((t-u) * const))
+            m = m[:,newaxis]
             return m*c1 + (1-m)*c2
+            
+            
+            
     return mix
     
 def main():
