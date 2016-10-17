1b4d842f7639cae4efe6a5b0e65b8e1c18a5e9a1
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-17 19:11:09 +0200

    refactored
    Refactored code to remove some code duplication. Also tweaked some
    parameters for more appropriate image quality and render time.

--- readme_renders.py
+++ readme_renders.py
@@ -3,55 +3,49 @@
 import taylorbundle
 import curve
 from numpy import (sin, cos, pi)
+from copy import copy
 
-x = lambda t: t
-f = lambda t: sin(t)
-c = curve.Curve(x, f)
-tb1 = taylorbundle.TaylorBundle( curve = c )
+wx = 8*pi
+wy = wx * 9./32
+tb1 = taylorbundle.TaylorBundle( 
+          curve = curve.fromFunction(sin)
+        , window = [0, wx, -wy, wy]
+        , domain = [0,8*pi]
+        , curvelw = 6
+        , tanlen = 30
+        , dpi = 30
+        )
 
 def fig1():
-    tb1.set_options( filename = "figures/readme_fig1"
-                   , n_tan = 4
-                   , window = [0, 16, -4.5, 4.5]
-                   , domain = [0,18]
-                   , curvelw = 6
-                   , tanlw = 4
-                   , tanalpha = 1
-                   , tanlen = 20
-                   , dpi = 30
-                   )
-    tb1.render()
+    tb = copy(tb1)
+    tb.set_options(
+              filename = "figures/readme_fig1"
+            , n_tan = 4
+            , tanlw = 4
+            , tanalpha = 1
+            )
+    tb.render()
 
 def fig2():
-    wx = 8*pi
-    wy = wx * 9./32
-    tb1.set_options( filename = "figures/readme_fig2"
-                   , n_tan = 400
-                   , window = [0, wx, -wy, wy]
-                   , domain = [0,8*pi]
-                   , curvelw = 6
-                   , tanlw = 2
-                   , tanalpha = 0.4
-                   , tanlen = 20
-                   , dpi = 30
-                   )
-    tb1.render()
+    tb = copy(tb1)
+    tb.set_options(
+              filename = "figures/readme_fig2"
+            , n_tan = 200
+            , tanlw = 4
+            , tanalpha = 0.5
+            )
+    tb.render()
 
 def fig3():
-    wx = 8*pi
-    wy = wx * 9./32
-    tb1.set_options( filename = "figures/readme_fig3"
-                   , n_tan = 1000
-                   , n_part = 20
-                   , window = [0, wx, -wy, wy]
-                   , domain = [0,8*pi]
-                   , curvelw = 6
-                   , tanlw = 01
-                   , tanalpha = 0.4
-                   , tanlen = 20
-                   , dpi = 30
-                   )
-    tb1.render()
+    tb = copy(tb1)
+    tb.set_options(
+              filename = "figures/readme_fig3"
+            , n_tan = 1000
+            , n_part = 10
+            , tanlw = 1
+            , tanalpha = 0.4
+            )
+    tb.render()
 
 if __name__ == "__main__":
     fig1()
