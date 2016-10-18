894f80c54faeef0524ae6ddb8cc0efd9be3f353c
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-18 20:51:47 +0200

    updated readme
    Redid figure 1. Did figures 4 and 5. Fixed some numbers and wordings.

--- readme_renders.py
+++ readme_renders.py
@@ -5,12 +5,15 @@ import curve
 from numpy import (sin, cos, pi)
 from copy import copy
 
-wx = 8*pi
-wy = wx * 9./32
-tb1 = taylorbundle.TaylorBundle( 
+####################
+##  Tangent bundle
+
+wx = 4*pi
+wy = wx * 9./16
+tb1 = taylorbundle.TaylorBundle(
           curve = curve.fromFunction(sin)
-        , window = [0, wx, -wy, wy]
-        , domain = [0,8*pi]
+        , window = [-wx, wx, -wy, wy]
+        , domain = [-wx, wx]
         , curvelw = 6
         , tanlen = 30
         , dpi = 30
@@ -20,7 +23,8 @@ def fig1():
     tb = copy(tb1)
     tb.set_options(
               filename = "figures/readme_fig1"
-            , n_tan = 4
+            , n_tan = 1
+            , bundledomain = [1,1]
             , tanlw = 4
             , tanalpha = 1
             )
@@ -47,7 +51,38 @@ def fig3():
             )
     tb.render()
 
+##########################################
+##  2nd degree taylor bundle
+
+tb2 = copy(tb1)
+tb2.set_options( degree = 2 )
+
+def fig4():
+    tb = copy(tb2)
+    tb.set_options(
+              filename = "figures/readme_fig4"
+            , n_tan = 1
+            , bundledomain = [1,1]
+            , tanlw = 4
+            , tanalpha = 1
+            )
+    tb.render()
+
+def fig5():
+    tb = copy(tb2)
+    tb.set_options(
+              filename = "figures/readme_fig5"
+            , n_tan = 1000
+            , n_part = 10
+            , tanlw = 1
+            , tanalpha = 0.4
+            )
+    tb.render()
+
+
 if __name__ == "__main__":
     fig1()
     fig2()
     fig3()
+    fig4()
+    fig5()
