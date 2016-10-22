690fa5f1c54bb300a469fb4f247db8a49be90e43
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-22 21:03:23 +0200

    new usage example: asteroid
    demonstrating the use of a user-defined curve, and comparing it's
    numerically differetiated bundle to the analogous pre-defined curve with
    its exact differetiation.

--- example_asteroid.py
+++ example_asteroid.py
@@ -1,33 +1,50 @@
-# Example of taylor bundle rendering. Asteriod-like hypotrochoid curve, with it's 5th degree TB.
+# Example of taylor bundle rendering. Asteriod-like hypotrochoid curve,
+# with it's 5th degree TB. First it's user-defined and rendered with the
+# default, numeric differentiation. Then it's rendered with the predefined
+# Trochoid class, with exact differentiation. In this case, it is evident
+# that numerical differnetiation makes no difference on the result.
 
 import curve
 import taylorbundle
 import colormix
-import numpy
+from numpy import sin, cos, pi
 
-tau = 2 * numpy.pi
+tau = 2*pi  # the correct circle constant
 
-def main():
-    c = curve.Trochoid(-4, 1./3, 0)
-    color1 = (0.2, 0.4, 1)
-    color2 = (1, 0, 0.2)
-    color_f = colormix.cosine2(color1, color2, 0, tau/4)
-    tb = taylorbundle.TaylorBundle(
-          curve = c
-        , showcurve = False
-        , degree = 5
-        , window = [-4, 4, -2.25, 2.25]
-        , tancol = color_f
-        , tanlw = 0.4
-        , tanalpha = 0.2
-        , n_part = 8
-        # , keep_partials = True
-        , filename = "renders/asteroid"
-        )
-    tb.render(
-          # preview = True
-        # , scale = 0.25
-        )
-    
-if __name__ == "__main__":
-    main()
+# first, user-defined Curve with numeric differentiation
+
+def x(t): return cos(t) + cos(-3*t) / 3.0
+def y(t): return sin(t) + sin(-3*t) / 3.0
+c_num = curve.Curve(x, y)
+
+color1 = (0.2, 0.4, 1)  # clear blue
+color2 = (1, 0, 0.2)    # red
+mix = colormix.cosine2(color1, color2, 0, tau/4)
+
+tb = taylorbundle.TaylorBundle(
+      filename = "renders/example_asteroid_numericdiff"
+    , curve = c_num
+    , domain = [0, tau]
+    , showcurve = False
+    , degree = 5
+    , window = [-4, 4, -2.25, 2.25]
+    , figsize = [16,9]
+    , dpi = 30
+    , n_part = 8
+    , n_tan = 750
+    , tancol = mix
+    , tanlw = 0.5
+    , tanalpha = 0.4
+    , tandomain = [-2,2]
+    )
+tb.render()
+
+# Then, predefined curve with exact differentiation
+
+c_exact = curve.Trochoid(-4, 1./3, 0)
+
+tb.set_options(
+      filename = "renders/example_asteroid_exactdiff"
+    , curve = c_exact
+    )
+tb.render()
