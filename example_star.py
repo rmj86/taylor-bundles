1c9e0d11560217e274fbbdf1ab223a311bf32f0f
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-22 20:17:43 +0200

    new usage example: star
    demonstrating basic usage of a predefined curve and colormixer

--- example_star.py
+++ example_star.py
@@ -9,27 +9,26 @@ from numpy import pi, sqrt
 
 tau = 2 * pi
 
-def main():
-    c = curve.Trochoid(-5, r= 14./240)
-    color1 = (1, 0.2, 0.1)
-    color2 = (0.2, 0.0, 0.0)
-    color_f = colormix.cosine2(color1, color2, 0, tau/10)
-    tb = taylorbundle.TaylorBundle(
-          curve = c
-        , showcurve = False
-        , degree = 2
-        , window = [-5/sqrt(2), 19/sqrt(2), -4, 20]
-        , figsize = [8.267, 11.692]
-        , facecolor = 'w'
-        , tancol = color_f
-        , tandomain = [-24, 24]
-        , filename = "renders/example_star"
-        )
+c = curve.Trochoid(-5, r= 14./240)
+color1 = (1, 0.2, 0.1) # lightish red
+color2 = (0.2, 0, 0)   # reddish black
+# periodic gradient with color1 around t=0 and color2 around t=tau/10
+mix = colormix.cosine2(color1, color2, 0, tau/10)
+tb = taylorbundle.TaylorBundle(
+      filename = "renders/example_star"
+    , curve = c
+    , showcurve = False
+    , window = [-5/sqrt(2), 19/sqrt(2), -4, 20]
+    , figsize = [8.267, 11.692]   # size of an A4 paper
+    , facecolor = 'w'
+    , n_part = 8
+    , n_tan = 500
+    , tancol = mix
+    , tanlw = 0.5
+    , tanalpha = 0.4
+    , tandomain = [-24, 24]
+    , degree = 2
+    , dpi = 30
+    )
 
-    tb.render( 
-          # preview = True
-        # , scale = 0.25
-        )
-    
-if __name__ == "__main__":
-    main()
+tb.render()
