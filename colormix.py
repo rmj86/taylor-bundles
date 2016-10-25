7d52c35d91eb6f1d513cba783491a46d56eb1b3d
rmj86 <johannes.rehnstrom.1986@gmail.com>
2016-10-25 12:27:26 +0200

    new tests for colormix
    renamed map -> norm. "normalization" is the term used in matplotlib for
    this concept.
    
    Added `_bounded(t)`, a utility function for `mix2`. Bounded truncates
    any values in t outside of the domain [0,1]. Values outside of this
    domain could cause matplotlib to be given nonsensical color values and
    raise an error.
    
    added `normalize(u,v)`, a linear transformation from the domain [u,v] to
    [0,1]. Used as a utility is some other normalizations, but also has
    usage in its own right.
    
    Added `colormap(cmap_name, norm)`. takes a matplotlib colormap name and
    a norm, returns the expected color function.

--- colormix.py
+++ colormix.py
@@ -2,30 +2,52 @@
 
 from numpy import pi, cos, sqrt, array, newaxis, full, exp, log
 from matplotlib.colors import colorConverter
+from matplotlib.cm import get_cmap
 from types import FunctionType
 
 tau = 2*pi
 
 def fromConstant(color):
-    if type(color) == FunctionType: # assume its valid and return unchanged
+    """ Takes any valid color argument, and returns a corresponding
+        color function. Constants are tuirned into constant
+        functions. Functions are returned unchanged. """
+    if type(color) == FunctionType:
         return color
-    else:  # it is a constant
+    else:
         c_arr = colorConverter.to_rgba_array(color)
         def const_color(t):
             return full(t.shape+(4,), c_arr)
         return const_color
 
-def mix2(color1, color2, map, linear=True):
+# Helper function for mix2. Guarantees that the norm parameter doesn't
+#exceed the interval [0,1]; if it did, the rgba color values out may
+# exceed [0,1], which makes matplotlib throw an error.
+def _bounded(t):
+    """ Takes an ndarray `t` as input, and returns it with
+        values bounded to the interval [0,1]. Values in t<0
+        are set to 0, and values in t>1 are set to 1.
+        Mutates ``t"""
+    t[t<0] = 0
+    t[t>1] = 1
+    return t
+
+def colormap(cmap_name, norm):
+    cmap = get_cmap(cmap_name)
+    def colorfunc(t):
+        return cmap(norm(t))
+    return colorfunc
+
+def mix2(color1, color2, norm, linear=True):
     """ retuns a function mix(t) which takes an array of curve
-        parameters and returns an array of rgba-tuples. map is
+        parameters and returns an array of rgba-tuples. norm is
         a function from the curve parameter t to values in the
-        range [0,1]. Where the map is 0, shows color1, where
-        the map is 1, shows color2.
+        range [0,1]. Where the norm is closer to 0, shows more
+        of color1. Where closer to 1, more of color2.
         When linear==True, mix in a linear color space."""
     cfunc1 = fromConstant(color1)
     cfunc2 = fromConstant(color2)
     def mix(t):
-        m = map(t)[:,newaxis]
+        m = _bounded(norm(t))[:,newaxis]
         c1 = cfunc1(t)
         c2 = cfunc2(t)
         if linear:
@@ -34,34 +56,43 @@ def mix2(color1, color2, map, linear=True):
             return (1-m)*c1 + m*c2
     return mix
 
+## Normalization functions 
+
+def normalize(u, v):
+    """returns a function f that linearly transforms
+        the interval [u,v] to [0,1]. """
+    def _norm(t):
+        return (t-u)/float(v-u)
+    return _norm
+
 def smoothstep(u, v):
     """ A smooth step from 0 to 1 in the domain [u,v]. When
         t<u, is constant 0. When v<t, is constant 1."""
-    def step(t):
-        ts = (t-u)/(v-u)  # scale t so that [u,v] becomes [0,1]
+    norm = normalize(u, v)
+    def _smoothstep_norm(t):
+        ts = norm(t)  # scale t so that [u,v] becomes [0,1]
         m = ts*ts*(3-2*ts)
         m[ts<0] = 0
         m[ts>1] = 1
         return m
-    return step
+    return _smoothstep_norm
 
 def cosine(p1, p2):
     """ A cosine curve oscillating between 0 and 1. It is 0
         where t=p1, 1 where t=p2, and has period 2*(p2-p1). """
-    period = 2*(p2-p1)
-    const = tau/period # ???
-    def oscillator(t):
-        ts = pi/(p2-p1) * (p1-t) # scale t so [p1,p2] becomes [0,pi]
+    norm = normalize(p1, p2)
+    def _cosine_norm(t):
+        ts = pi * norm(t)  # scale t so [p1,p2] becomes [0,pi]
         m = 0.5 * (1 - cos(ts))
         return m
-    return oscillator
+    return _cosine_norm
 
 def gaussian(c, w):
     """ A gaussian curve with it's peak at c and width w.
         Width refers to it's "full width at half maximum"."""
     s = 2*sqrt(log(2)) / w  # scaling factor for t
-    def bellcurve(t):
+    def _gaussian_norm(t):
         ts = (t-c)*s  # t scaled so that [c-w, c+w] is FWHM range
         m = exp(-ts*ts)
         return m
-    return bellcurve
+    return _gaussian_norm
